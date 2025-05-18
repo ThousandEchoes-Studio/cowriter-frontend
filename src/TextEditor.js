c66827de58e5c603f2545243b317029af41ca30c
ThousandEchoes-Studio <josh@thousandechoes.com>
2025-05-17 21:13:13 -0700

    Update TextEditor.js
    Add simplified TextEditor component

--- src/TextEditor.js
+++ src/TextEditor.js
@@ -1,1 +1,33 @@
-import 'react-quill/dist/quill.snow.css';
+import React, { useState } from 'react';
+import './TextEditor.css';
+
+function TextEditor() {
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+
+  return (
+    <div className="editor-container">
+      <div className="editor-header">
+        <input
+          type="text"
+          placeholder="Document Title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="title-input"
+        />
+        <button className="save-button">
+          Save
+        </button>
+      </div>
+      
+      <textarea 
+        value={content}
+        onChange={(e) => setContent(e.target.value)}
+        className="simple-editor"
+        placeholder="Start writing here..."
+      />
+    </div>
+  );
+}
+
+export default TextEditor;
